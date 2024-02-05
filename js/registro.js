be98bc2840090e6536ff000ba226cb083958be00
MariaKuenneth <mariakuenneth@gmail.com>
2024-02-05 03:54:59 -0300

    solicitud de datos lista. Falta responsive

--- js/registro.js
+++ js/registro.js
@@ -3,9 +3,13 @@
 function mostrar(seleccionado){
     if (seleccionado.value == "paciente") {
         document.getElementById('paciente').classList.remove('esconder');
+        document.getElementById('medico').classList.add('esconder'); 
+
     }else{
         if (seleccionado.value == "medico") {
-            document.getElementById('medico').classList.remove('esconder');            
+            document.getElementById('medico').classList.remove('esconder');
+            document.getElementById('paciente').classList.add('esconder');
+         
         }else{
             document.getElementById('paciente').classList.add('esconder');
             document.getElementById('medico').classList.add('esconder');            
